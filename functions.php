f44672de28fb93d77eaa9c16a0e4c1e1355a4800
FrancySanchez <francy.sanchez@gmail.com>
2017-02-19 19:27:14 +0000

    author
    *social icon
    * tamaño de letra del post

--- functions.php
+++ functions.php
@@ -273,3 +273,17 @@ function activello_header_search_filter($form){
     $form = '<form action="'.esc_url( home_url( "/" ) ).'" method="get"><input type="text" name="s" value="'.get_search_query().'" placeholder="'. esc_attr_x( __('Search', 'activello'), 'search placeholder', 'activello' ).'"><button type="submit" class="header-search-icon" name="submit" id="searchsubmit" value="'. esc_attr_x( 'Search', 'submit button', 'activello' ).'"><i class="fa fa-search"></i></button></form>';
     return $form;
 }
+
+
+/*===================================================================================
+ * Add Author Links
+ * =================================================================================*/
+function add_to_author_profile( $contactmethods ) {
+  
+  $contactmethods['instagram_profile'] = 'Instagram Profile URL';
+  $contactmethods['linkedin_profile'] = 'Linkedin Profile URL';
+  
+  return $contactmethods;
+}
+add_filter( 'user_contactmethods', 'add_to_author_profile', 10, 1);
+
